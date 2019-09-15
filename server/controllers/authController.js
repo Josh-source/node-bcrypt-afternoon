7158d9a9145a9f1477336cfe68dd3ef8f28f9673
Joshua Riley <Joshy@Faiths-MacBook-Pro.local>
2019-09-14 19:27:35 -0500

    fixed logout in AuthController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -29,5 +29,9 @@ module.exports={
         }
         req.session.user = { isAdmin: user.is_admin, id: user.id, username: user.username};
         return res.send(req.session.user);
+    },
+    logOut:( req, res) => {
+        req.session.destroy();
+        return res.sendStatus(200);
     }
 };
